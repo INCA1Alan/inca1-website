2fd760253ce64af29c343c8bcdea7c2c72f2b8a1
wychwood <maciej@thewychwood.ca>
2014-11-12 18:22:25 -0500

    site nav

--- scripts/site.js
+++ scripts/site.js
@@ -1,45 +1,56 @@
 /*
-  Create a new YUI instance and load the node
-  and event modules.
+    Create a new YUI instance and load the node
+    and event modules.
 
-  HELPFUL LINKS
-  =============
+    HELPFUL LINKS
+    =============
 
-  http://yuilibrary.com/yui/docs/api/
-  http://www.jsrosettastone.com/
+    http://yuilibrary.com/yui/docs/api/
+    http://www.jsrosettastone.com/
 */
 
 YUI().use('node', 'event', 'squarespace-util', function (Y) {
-  Y.on('domready', function () {
+    Y.on('domready', function () {
 
-    /*
-      This function loads and refreshes images
-      using Squarespace's responsive ImageLoader.
-    */
+        /*
+            This function loads and refreshes images
+            using Squarespace's responsive ImageLoader.
+        */
 
-    function loadImages () {
-      Y.all('img[data-src]' ).each(function(img) {
-        ImageLoader.load(img);
-      });
-    }
+        function loadImages () {
+            Y.all('img[data-src]' ).each(function(img) {
+                ImageLoader.load(img);
+            });
+        }
 
-    /*
-      ImageLoader will refresh images on windowresize
-      and any change in the Style Editor.
-    */
+        /*
+            ImageLoader will refresh images on windowresize
+            and any change in the Style Editor.
+        */
 
-    Y.one(window).on('resize', loadImages, this);
-    Y.Global && Y.Global.on('tweak:change', loadImages, this);    
-
-
-
-    /*
-      Author's custom code goes here.
-      =============================== */
+        Y.one(window).on('resize', loadImages, this);
+        Y.Global && Y.Global.on('tweak:change', loadImages, this);        
 
 
 
-  });
+        /*
+            Author's custom code goes here.
+            =============================== */
+
+        var $html = Y.one('html');
+
+        var $menuOpen = Y.one('.menu__open');
+        var $menuClose = Y.one('.menu__close');
+
+        $menuOpen.on('click', function() {
+            $html.addClass('show-menu');
+        });
+
+        $menuClose.on('click', function() {
+            $html.removeClass('show-menu');
+        });
+
+    });
 });
 
 
