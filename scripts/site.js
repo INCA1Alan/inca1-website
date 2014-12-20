49c339167b3ad34d83818569b33ad3a0c59c2253
Maciej J <maciej@playgroundinc.com>
2014-12-19 19:41:42 -0500

    revert viewportSize stuff, make h.264/aac video first priority

--- scripts/site.js
+++ scripts/site.js
@@ -1,60 +1,3 @@
-/*! viewportSize | Author: Tyson Matanich, 2013 | License: MIT */
-(function (window) {
-
-	window.viewportSize = {};
-
-	window.viewportSize.getHeight = function () {
-		return getSize("Height");
-	};
-
-	window.viewportSize.getWidth = function () {
-		return getSize("Width");
-	};
-
-	var getSize = function (Name) {
-		var size;
-		var name = Name.toLowerCase();
-		var document = window.document;
-		var documentElement = document.documentElement;
-		if (window["inner" + Name] === undefined) {
-			// IE6 & IE7 don't have window.innerWidth or innerHeight
-			size = documentElement["client" + Name];
-		}
-		else if (window["inner" + Name] != documentElement["client" + Name]) {
-			// WebKit doesn't include scrollbars while calculating viewport size so we have to get fancy
-
-			// Insert markup to test if a media query will match document.doumentElement["client" + Name]
-			var bodyElement = document.createElement("body");
-			bodyElement.id = "vpw-test-b";
-			bodyElement.style.cssText = "overflow:scroll";
-			var divElement = document.createElement("div");
-			divElement.id = "vpw-test-d";
-			divElement.style.cssText = "position:absolute;top:-1000px";
-			// Getting specific on the CSS selector so it won't get overridden easily
-			divElement.innerHTML = "<style>@media(" + name + ":" + documentElement["client" + Name] + "px){body#vpw-test-b div#vpw-test-d{" + name + ":7px!important}}</style>";
-			bodyElement.appendChild(divElement);
-			documentElement.insertBefore(bodyElement, document.head);
-
-			if (divElement["offset" + Name] == 7) {
-				// Media query matches document.documentElement["client" + Name]
-				size = documentElement["client" + Name];
-			}
-			else {
-				// Media query didn't match, use window["inner" + Name]
-				size = window["inner" + Name];
-			}
-			// Cleanup
-			documentElement.removeChild(bodyElement);
-		}
-		else {
-			// Default to use window["inner" + Name]
-			size = window["inner" + Name];
-		}
-		return size;
-	};
-
-})(this);
-
 /*
     Create a new YUI instance and load the node
     and event modules.
@@ -130,8 +73,8 @@ YUI().use('node', 'event', 'squarespace-util', function (Y) {
             var headerHeight = $header.height();
 
             function sizeVideo() {
-                var w = viewportSize.getWidth();
-                var h = viewportSize.getHeight();
+                var w = window.innerWidth;
+                var h = window.innerHeight;
 
                 introHeight = h;
                 headerHeight = $header.height();
